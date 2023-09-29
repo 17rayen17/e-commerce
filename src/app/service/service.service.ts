f0378cf3651fd0c8a759000326cdcd4f30b3e97c
rayen <rayen.andolsi98@gmail.com>
2023-09-29 10:11:08 +0100

    first commit

--- src/app/service/service.service.ts
+++ src/app/service/service.service.ts
@@ -1,19 +1,20 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ServiceService {
-
+  API = environment.BASE_URL;
   constructor(private http : HttpClient) { }
 
   addcart(data : any): Observable<any> {
-    return this.http.post("http://localhost:3000/shop",data);
+    return this.http.post(this.API,data);
   }
   getcart(): Observable<any> {
-    return this.http.get("http://localhost:3000/shop");
+    return this.http.get(this.API);
   }
 
 }
